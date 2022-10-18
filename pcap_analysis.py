a9fe7a444be726ac7416686187d38596113c06cf
kuciniak1 <01159031@pw.edu.pl>
2022-10-18 20:32:58 +0200

    added logger to pcap analysis

--- pcap_analysis.py
+++ pcap_analysis.py
@@ -1,19 +1,25 @@
+from modules import logger
 import libpcap
+
 
 def traffic_from_file_libpcap(path_to_file, filter):
     try:
+        logger.debug("Trying to open: %s", path_to_file)
         traffic = libpcap.open_offline(offline = path_to_file, filter = filter)
+        logger.debug("Filter applied: %s", filter)
         for entry in traffic:
             print(entry)
     except Exception as exception:
-        print('Invalid path to file')
+        logger.error('Error while opening file: %s', path_to_file)
 
 import pyshark
 
 def traffic_from_file_pyshark(path_to_file, filter):
-    try:   
+    try:
+        logger.debug("Trying to open: %s", path_to_file)
         traffic = pyshark.FileCapture(path_to_file, display_filter=filter)
+        logger.debug("Filter applied: %s", filter)
         for entry in traffic:
             print(entry)
     except Exception as exception:
-        print('Invalid path to file')
+        logger.error('Error while opening file: %s', path_to_file)
