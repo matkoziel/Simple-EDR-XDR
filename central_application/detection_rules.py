d90657c41bf0b5a94994c99b4738d442ea47ef8b
-global <-global>
2022-10-27 15:53:47 +0200

    Read detection rules, change logging filename

---
+++ central_application/detection_rules.py
@@ -0,0 +1,24 @@
+
+def funkcja_testowa(**kwargs):
+# ciało funkcji - właściwa reguła operująca na danych z args
+# procesowanie pcap
+# for pcap in kwargs[pcap]:
+# procesowanie evtx
+# for evtx in kwargs[evtx]:
+# procesowanie xml
+# for xml in kwargs[xml]:
+# procesowanie json
+# for json in kwargs[json]:
+# procesowanie txt
+# for txt in kwargs[txt]:
+# ostateczna reguła - tj. co ma się wykonać
+    if True==True:
+        action_alert = "..." # akcja: "local", "remote"
+        description = "Alert ..."
+    else:
+        action_alert = None
+        description = None
+    return action_alert, description
+
+def funkcja_2(**kwargs):
+    print("Dupa")
