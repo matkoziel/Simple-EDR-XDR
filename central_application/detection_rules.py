c827a793662e062ad323f963e3dc1800b297bafa
matkoziel <01159025@pw.edu.pl>
2022-11-01 20:21:27 +0100

    Processing pcap, txt and evtx in detection rules

--- central_application/detection_rules.py
+++ central_application/detection_rules.py
@@ -1,22 +1,35 @@
+import re 
+import pcap_analysis
+import Evtx.Evtx as evtx 
 
 def funkcja_testowa(**kwargs):
-    print(kwargs)
-# ciało funkcji - właściwa reguła operująca na danych z args
-# procesowanie pcap
+
+    kwargs["pcap"]=pcap_analysis.traffic_from_file_pyshark(kwargs["pcap"],"")
     for pcap in kwargs["pcap"]:
-        print(pcap)
         pass
-    for evtx in kwargs["evtx"]:
-        # procesowanie xml
-        pass
+
+    with evtx.Evtx(kwargs["evtx"]) as event_log:
+        res=[]
+        for record in event_log.records():
+            res.append(record.xml())
+        kwargs["evtx"]=res
+    for evtx_ in kwargs["evtx"]:
+        print(evtx_)
+
     for xml in kwargs["xml"]:
         # procesowanie json
         pass
+
     for json in kwargs["json"]:
         # procesowanie txt
         pass
+    
+    with open(kwargs["txt"],"r") as txt_file:
+        kwargs["txt"]=txt_file.readlines()
+        txt_file.close()
     for txt in kwargs["txt"]:
-        pass
+        if re.search("error",txt,re.IGNORECASE):
+            print("ERROR")
 
     if True==True:
         action_alert = "..." # akcja: "local", "remote"
