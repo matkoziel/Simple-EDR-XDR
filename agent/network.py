abbf7b375c0459cf3f7e3175cafaaa4fece253f9
kuciniak1 <01159031@pw.edu.pl>
2022-11-05 23:52:31 +0100

    remote agent - network

--- agent/network.py
+++ agent/network.py
@@ -1,10 +1,22 @@
 import ifcfg
-import json
+import os
+import pyshark
+
+pcaps_archive = "/var/log/agent/pcaps"
 
 def get_network_config():
-    print(ifcfg.interfaces())
     return ifcfg.interfaces()
 
+def sniff(interface, filter, file_name, sniff_time):
+    capture = pyshark.LiveCapture(interface=interface, bpf_filter=filter, output_file=(pcaps_archive + file_name))
+    capture.sniff(sniff_time)
+    return (pcaps_archive + file_name)
 
-def sniffing():
-    
+def get_pcaps():
+    return os.listdir(pcaps_archive)
+
+def get_specific_pcap(pcap_name):
+    if os.path.isfile(pcaps_archive+pcap_name):
+        return(pcaps_archive+pcap_name)
+    return -1
+
