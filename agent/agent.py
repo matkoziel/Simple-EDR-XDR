73e1de25a0c4a14ba1471499b63fd0d687d0c921
kuciniak1 <01159031@pw.edu.pl>
2022-11-05 23:52:48 +0100

    remote agent - api start

---
+++ agent/agent.py
@@ -0,0 +1,42 @@
+from flask import Flask, jsonify, request, send_file
+from flask_restful import Resource, Api
+
+import logs
+import network
+import remote_control
+
+class execute_command(Resource):
+    def post(self):
+        json_data = request.form
+        output = remote_control.execute_command(json_data['command'])
+        return jsonify(command = json_data['command'], output = output)
+
+class get_log_list(Resource):
+    def get(self):
+        return logs.get_logs()
+
+class get_chosen_logs(Resource):
+    def get(self):
+        print('to do')
+
+class get_network_config(Resource):
+    def get(self):
+        return network.get_network_config()
+
+class get_pcaps_list(Resource):
+    def get(self):
+        return network.get_pcaps()
+
+class get_chosen_pcaps(Resource):
+    def get(self):
+        print('to do')
+
+class sniffing_mode(Resource):
+    def post(self):
+        json_data = request.form
+        interface = json_data['interface']
+        filter = json_data['filter']
+        file_name = json_data['file_name']
+        sniff_time = json_data['sniff_time']
+        network.sniff(interface, filter, file_name, sniff_time)
+        return jsonify(interface = interface, filter = filter, file_name = file_name, sniff_time = sniff_time)
