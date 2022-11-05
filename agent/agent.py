af494c0500a5700ae4ba02bff512be26dc2c23f2
kuciniak1 <01159031@pw.edu.pl>
2022-11-05 23:56:09 +0100

    agent - api

--- agent/agent.py
+++ agent/agent.py
@@ -31,7 +31,7 @@ class get_chosen_pcaps(Resource):
     def get(self):
         print('to do')
 
-class sniffing_mode(Resource):
+class sniffing(Resource):
     def post(self):
         json_data = request.form
         interface = json_data['interface']
@@ -40,3 +40,16 @@ class sniffing_mode(Resource):
         sniff_time = json_data['sniff_time']
         network.sniff(interface, filter, file_name, sniff_time)
         return jsonify(interface = interface, filter = filter, file_name = file_name, sniff_time = sniff_time)
+
+
+if __name__ == '__main__':
+    app = Flask(__name__)
+    api = Api(app)
+    api.add_resource(execute_command, '/execute_command')
+    api.add_resource(get_log_list, '/get_log_list')
+    api.add_resource(get_chosen_logs, '/get_chosen_logs')
+    api.add_resource(get_network_config, '/get_network_config')
+    api.add_resource(get_pcaps_list, '/get_pcaps_list')
+    api.add_resource(get_chosen_pcaps, '/get_chosen_pcaps')
+    api.add_resource(sniffing, '/sniffing')
+    app.run(host='0.0.0.0', port = 3000, debug=True)
