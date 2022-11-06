8545a125900e1082e99aba17dc7d269bc8d131eb
kuciniak1 <01159031@pw.edu.pl>
2022-11-06 17:30:02 +0100

    logging, capturing

--- agent/logs.py
+++ agent/logs.py
@@ -1,11 +1,13 @@
 import os
+from pathlib import Path
 
-logs_archive = "/var/log/agent/logs"
+logs_archive = os.environ.get('HOME') + "/.agent/logs/"
 
 def get_logs():
+    Path(logs_archive).mkdir(parents=True, exist_ok=True)
     return os.listdir(logs_archive)
 
 def get_specific_log(log_name):
     if os.path.isfile(logs_archive+log_name):
         return(logs_archive+log_name)
-    return "No such file: " + log_name
+    return -1
--- agent/network.py
+++ agent/network.py
@@ -1,18 +1,21 @@
 import ifcfg
 import os
 import pyshark
+from pathlib import Path
 
-pcaps_archive = "/var/log/agent/pcaps"
+pcaps_archive = os.environ.get('HOME') + "/.agent/pcaps/"
 
 def get_network_config():
     return ifcfg.interfaces()
 
 def sniff(interface, filter, file_name, sniff_time):
+    Path(pcaps_archive).mkdir(parents=True, exist_ok=True)
     capture = pyshark.LiveCapture(interface=interface, bpf_filter=filter, output_file=(pcaps_archive + file_name))
-    capture.sniff(sniff_time)
+    capture.sniff(timeout=sniff_time)
     return (pcaps_archive + file_name)
 
 def get_pcaps():
+    Path(pcaps_archive).mkdir(parents=True, exist_ok=True)
     return os.listdir(pcaps_archive)
 
 def get_specific_pcap(pcap_name):
